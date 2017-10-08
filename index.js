3fa7a9154efd55880854b1ecd3af981bb4c0507b
ezefranca <ezequiel.ifsp@gmail.com>
2017-10-08 16:24:11 -0300

    remove index tp server

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
+const list = require('lista-impropria-api');
+app.set('port', (process.env.PORT || 8080))
+app.use(express.static(__dirname + '/public'))
 
-app.set('port', (process.env.PORT || 5000));
+app.get('/testam/', function(request, response) {
+  testamEmAnimais(request, response);
+})
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.get('/naoTestam/', function(request, response) {
+  naoTestamEmAnimais(request, response);
+})
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
+
+function testamEmAnimais(request, response) {
+	list.testamEmAnimais().then(function(tabela) {
+	response.send(tabela);
+}, function(err){
+	response.send(err);
 });
+}
+
+function naoTestamEmAnimais(request, response) {
+	list.naoTestamEmAnimais().then(function(tabela) {
+	response.send(tabela);
+}, function(err){
+	response.send(err);
+});
+// }
